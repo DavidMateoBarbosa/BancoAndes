74087148b5369d45eec55e179b617732a839e4eb
David Mateo Barbosa Monsalve <d.barbosam@uniandes.edu.co>
2024-05-26 00:52:17 -0500

    SetUp

---
+++ src/main/java/uniandes/edu/co/application/BancoAndesApplication.java
@@ -0,0 +1,11 @@
+package uniandes.edu.co.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoAndesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BancoAndesApplication.class, args);
+    }
+}
